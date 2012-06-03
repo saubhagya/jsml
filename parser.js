7d23e9e5d2282683dde62b78734eee154f0eebbd
Saubhagya <saubhagyamaheshwari@gmail.com>
2012-06-03 12:04:02 +0530

    Initial commit.

---
+++ parser.js
@@ -0,0 +1,42 @@
+(function() {
+   "use strict";
+
+    var parseToHtml = function(obj, tag) {
+	var el = document.createElement(tag);
+
+        for (var key in obj) {
+            var val = obj[key];
+
+	    if (key === 'content')
+                el.innerHTML = val;
+	    else if (typeof val === 'object')
+                el.appendChild(parseToHtml(val, key));
+            else
+		el.setAttribute(key, val);
+	}
+	return el;
+    };
+
+    var processData = function(data) {
+        document.body = parseToHtml(JSON.parse(data), 'body');
+    };
+
+    var fetchData = function() {
+        var xhr = new XMLHttpRequest(),
+	    path = '.' + location.pathname;
+
+	if (path === './')
+	    path = './index';
+
+        xhr.open('GET', path + '.json', true);
+	xhr.send();
+	xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                processData(this.responseText);
+	    }
+	};
+    };
+
+    fetchData();
+})();
+
